abc8574fdd5f2df96e6da6e15e4adf319279194c
Siddhartha Vale <57783907+SiddharthaVale@users.noreply.github.com>
2020-02-12 18:26:59 -0500

    Add files via upload

---
+++ INFO5100 - App Dev/Lectures/Example.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1    ;
+
+// @author Samsung
+public class JavaApplication1 {
+
+    // @param args the command line arguments
+ public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is a simple java program. ");
+        /* This is a simple java program.
+           Call this file "Example.java".
+        */
+    }
+    
+}
